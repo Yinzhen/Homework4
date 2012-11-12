f598051ce1f02403f48c33b7c574aa62ac9f8004
Yinzhen Jin <yinzhenj@princeton.edu>
2012-11-12 16:04:19 -0500

    Delete one function deleteT()

--- temperature.h
+++ temperature.h
@@ -7,8 +7,6 @@ double ** new_Temperature(int nx, double dx);
 
 void print2file(double **T, int nx, string file);
 
-void deleteT(double ** T, int nx);
-
 void copyT(double ** T_p, double ** T_c, int nx);
 
 #endif // Temperature
